3f02fc819435f1b00b7af78627268e88daded35a
athyna-ai <athenahannah02@gmail.com>
2025-04-07 05:03:02 +0800

    Register
    fixed register design

--- SwiftStock(Redo)/obj/Debug/net9.0/SwiftStock(Redo).AssemblyInfo.cs
+++ SwiftStock(Redo)/obj/Debug/net9.0/SwiftStock(Redo).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwiftStock(Redo)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6d16e61157f6f26086bf83b28321d9f56fc5fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c81d24ad9405203810f390c5d9b458050c6b7e61")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwiftStock(Redo)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwiftStock(Redo)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
