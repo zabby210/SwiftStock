029715b8b36184009035f86b93f98bfe4d07baaf
athyna-ai <athenahannah02@gmail.com>
2025-04-08 19:33:33 +0800

    logo img
    updated

--- SwiftStock(Redo)/obj/Debug/net9.0/SwiftStock(Redo).AssemblyInfo.cs
+++ SwiftStock(Redo)/obj/Debug/net9.0/SwiftStock(Redo).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwiftStock(Redo)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65142a51a8cd80d7f35d8fab355ee90396b7fb95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45fc0ee63ddbf238b504b0b77044638fc36518ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwiftStock(Redo)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwiftStock(Redo)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SwiftStock(Redo)/obj/Release/net9.0/SwiftStock(Redo).AssemblyInfo.cs
+++ SwiftStock(Redo)/obj/Release/net9.0/SwiftStock(Redo).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwiftStock(Redo)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3342dd0aaba47cd7fe60c2ac82f8bee3966d3cf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45fc0ee63ddbf238b504b0b77044638fc36518ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwiftStock(Redo)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwiftStock(Redo)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
